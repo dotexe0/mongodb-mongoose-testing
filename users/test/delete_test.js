fb3080689806d710b72f56b825c4e8614acd1a77
Daniel Varela <dvarela@wiser.com>
2018-10-24 10:33:00 -0700

    formatting eslint / prettier

--- users/test/delete_test.js
+++ users/test/delete_test.js
@@ -1,32 +1,29 @@
-const assert = require('assert')
-const User = require('../src/users')
+const assert = require("assert");
+const User = require("../src/users");
 
-describe('Deleting a user', () => {
-  let daniel
+describe("Deleting a user", () => {
+  let daniel;
 
   beforeEach(done => {
-    daniel = new User({ name: 'Daniel'})
-    daniel.save().then(() => done())
-  })
+    daniel = new User({ name: "Daniel" });
+    daniel.save().then(() => done());
+  });
 
-  it('model instance remove', done => {
-   daniel.remove()
-    .then(() => User.findOne({ name: 'Daniel'}))
-    .then(user => {
-      assert(user === null)
-      done()
-    })
-  })
+  it("model instance remove", done => {
+    daniel
+      .remove()
+      .then(() => User.findOne({ name: "Daniel" }))
+      .then(user => {
+        assert(user === null);
+        done();
+      });
+  });
 
-  it('class method remove', () => {
-  // User
-  })
+  it("class method remove", () => {
+    // User
+  });
 
-  it('class method findAndRemove', () => {
+  it("class method findAndRemove", () => {});
 
-  })
-
-  it('class method findByIdAndRemove', () => {
-
-  })
-})
+  it("class method findByIdAndRemove", () => {});
+});
