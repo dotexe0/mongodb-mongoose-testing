43da7873f10fd26e8db3f8dcdb6a1bc8fc0ab719
Daniel Varela <dvarela@wiser.com>
2018-10-24 10:10:41 -0700

    delete test -- model instance removal

---
+++ users/test/delete_test.js
@@ -0,0 +1,32 @@
+const assert = require('assert')
+const User = require('../src/users')
+
+describe('Deleting a user', () => {
+  let daniel
+
+  beforeEach(done => {
+    daniel = new User({ name: 'Daniel'})
+    daniel.save().then(() => done())
+  })
+
+  it('model instance remove', done => {
+   daniel.remove()
+    .then(() => User.findOne({ name: 'Daniel'}))
+    .then(user => {
+      assert(user === null)
+      done()
+    })
+  })
+
+  it('class method remove', () => {
+  // User
+  })
+
+  it('class method findAndRemove', () => {
+
+  })
+
+  it('class method findByIdAndRemove', () => {
+
+  })
+})
