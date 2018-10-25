3e73ec592cbe415390bad2331b89622c4135fd51
Daniel Varela <dvarela@wiser.com>
2018-10-25 12:55:02 -0700

    formatting and fix test on read

--- users/test/read_test.js
+++ users/test/read_test.js
@@ -1,26 +1,27 @@
-const assert = require("assert");
-const User = require("../src/users");
+const assert = require('assert')
+const User = require('../src/users')
 
-describe("reading users from db", () => {
-  let daniel;
+describe('reading users from db', () => {
+  let daniel
   beforeEach(done => {
-    daniel = new User({ name: "Daniel" });
-    daniel.save().then(() => done());
-  });
-
-  it("finds all users with name Daniel", done => {
-    User.find({
-      name: "Daniel"
-    }).then(users => {
-      assert(users[0]._id.toString() === daniel._id.toString());
-      done();
-    });
-  });
-
-  it ('find a user with a particular id', done => {
-    User.findOne({ _id: daniel._id }).then(user => {
-      assert(user.name === 'daniel')
-    })
-    done()
+    daniel = new User({ name: 'Daniel' })
+    daniel.save().then(() => done())
   })
-});
+
+  it('finds all users with name Daniel', done => {
+    User.find({
+      name: 'Daniel'
+    }).then(users => {
+      assert(users[0]._id.toString() === daniel._id.toString())
+      done()
+    })
+  })
+
+  it('find a user with a particular id', done => {
+    User.findOne({ _id: daniel._id }).then(user => {
+      console.log('user', user)
+      assert(user.name === 'Daniel')
+      done()
+    })
+  })
+})
