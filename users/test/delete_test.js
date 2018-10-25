9bf6438f9c3524ee4fd1c4516e197b200fe61d6c
Daniel Varela <dvarela@wiser.com>
2018-10-25 12:37:24 -0700

    deleting a user using different mongoose methods

--- users/test/delete_test.js
+++ users/test/delete_test.js
@@ -1,29 +1,48 @@
-const assert = require("assert");
-const User = require("../src/users");
+const assert = require('assert')
+const User = require('../src/users')
 
-describe("Deleting a user", () => {
-  let daniel;
+describe('Deleting a user', () => {
+  let daniel
 
   beforeEach(done => {
-    daniel = new User({ name: "Daniel" });
-    daniel.save().then(() => done());
-  });
+    daniel = new User({ name: 'Daniel' })
+    daniel.save().then(() => done())
+  })
 
-  it("model instance remove", done => {
+  it('model instance remove', done => {
     daniel
       .remove()
-      .then(() => User.findOne({ name: "Daniel" }))
+      .then(() => User.findOne({ name: 'Daniel' }))
       .then(user => {
-        assert(user === null);
-        done();
-      });
-  });
+        assert(user === null)
+        done()
+      })
+  })
 
-  it("class method remove", () => {
-    // User
-  });
+  it('class method remove', done => {
+    User.remove({ name: 'Daniel' })
+      .then(() => User.findOne({ name: 'Daniel' }))
+      .then(user => {
+        assert(user === null)
+        done()
+      })
+  })
 
-  it("class method findAndRemove", () => {});
+  it('class method findOneAndRemove', done => {
+    User.findOneAndRemove({ name: 'Daniel' })
+      .then(() => User.findOne({ name: 'Daniel' }))
+      .then(user => {
+        assert(user === null)
+        done()
+      })
+  })
 
-  it("class method findByIdAndRemove", () => {});
-});
+  it('class method findByIdAndRemove', done => {
+    User.findByIdAndRemove(daniel._id)
+      .then(() => User.findOne({ name: 'Daniel' }))
+      .then(user => {
+        assert(user === null)
+        done()
+      })
+  })
+})
