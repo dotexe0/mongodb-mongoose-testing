32e5551892b0b94aa06f11af2b4e77137a1dff29
Daniel Varela <dotexe0@gmail.com>
2018-11-04 08:29:20 -0800

    association test

--- users/test/association_test.js
+++ users/test/association_test.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const mongoose = require('mongoose')
 const User = require('../src/users')
 const Comment = require('../src/comment')
@@ -6,7 +7,7 @@ const BlogPost = require('../src/blogPost')
 describe('Association', done => {
   let daniel, blogPost, comment
   beforeEach(done => {
-    daniel = new User({ name: 'Daniel ' })
+    daniel = new User({ name: 'Daniel' })
     blogPost = new BlogPost({ title: 'JS is awesome :D', content: 'Of course!' })
     comment = new Comment({ content: 'Best post of all time!! xoxoxo' })
 
@@ -20,9 +21,29 @@ describe('Association', done => {
   })
   
   it('saves a relation between user and blogpost', done => {
-    User.findOne({ name: 'Daniel '}).then(user => {
-      console.log('user', user)
+    User.findOne({ name: 'Daniel'}).populate('blogPosts').then(user => {
+      assert(user.blogPosts[0].title === 'JS is awesome :D')
       done()
     })
+  })
+
+  it('saves a full relation graph', done => {
+    User.findOne({ name: 'Daniel' }).populate({
+      path: 'blogPosts', //load up all associated blogposts
+      populate: {
+        path: 'comments', // load up all associated comments
+        model: 'comment', // tell it what model to use for association
+        populate: {
+          path: 'user', // load up associated user in comments
+          model: 'user' // tell it what model to use
+        }
+      }
+     }).then(user => {
+       assert(user.name === 'Daniel')
+       assert(user.blogPosts[0].title === 'JS is awesome :D')
+       assert(user.blogPosts[0].comments[0].content === 'Best post of all time!! xoxoxo')
+       assert(user.blogPosts[0].comments[0].user.name === 'Daniel')
+       done()
+     })
   })
 })
